fe0a26106c1d05da898a14a53e81b9907ddb360a
Le Hong Quan <lequan18042001@gmail.com>
2023-09-20 10:39:49 +0700

    ls18

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CatagoryController;
+use App\Http\Controllers\SPController;
+use App\Http\Controllers\BlogController;
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
+Route::get('/admins','AdminController@home');
+
+Route::get('/danhsach','CatagoryController@danhSachSanPham')->name('danh-sach-sp');
+Route::get('/blog','BlogController@danhSachBlog')->name('danh-sach-danh-muc-blog');
+Route::get('/danhmuc','SPController@danhMucSanPham')->name('danh-muc-sp');
+Route::get('/danhsachuser','UserController@danhSachUser')->name('danh-sach-user');
